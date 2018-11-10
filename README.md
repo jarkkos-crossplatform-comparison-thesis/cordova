ab9d5a2962faeb5eef1fcf10860bfec7244ffce6
Jarkko Saarinen <jsaarinen92@gmail.com>
2018-11-10 13:56:28 +0200

    Add ReactJS app

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # cordova
 Cordova version of the application
+
+# building & running
+Install npm, Cordova 7.1.0, ReactJS
+To run on Android:
+- cordova platform add android
+- yarn run build
+- cordova run android
---
+++ src/app/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import SelectTestScreen from "app/screens/selecttest";
+
+class App extends Component {
+  render() {
+    return <SelectTestScreen />
+  }
+}
+
+export default App;

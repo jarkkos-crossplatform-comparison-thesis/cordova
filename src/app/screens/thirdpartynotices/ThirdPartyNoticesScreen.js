37d6ac0a892fd4d55e7e75ee9eb11b0c3d7df29e
Jarkko Saarinen <jsaarinen92@gmail.com>
2018-12-11 21:57:24 +0200

    margin in the ThirdPartyNoticesScreen

--- src/app/screens/thirdpartynotices/ThirdPartyNoticesScreen.js
+++ src/app/screens/thirdpartynotices/ThirdPartyNoticesScreen.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react';
 
 import AppBody from "app/components/appBody";
+import 'app/App.css';
 
 class ThirdPartyNoticesScreen extends Component {
   render() {
     return (
       <AppBody>
-        <div>
-          <h3>Cordova</h3>
-          <a href="https://cordova.apache.org/">https://cordova.apache.org/</a>
-        </div>
+        <div className="horizontal-margin top-margin">
+          <div>
+            <h3>Cordova</h3>
+            <a href="https://cordova.apache.org/">https://cordova.apache.org/</a>
+          </div>
 
-        <div>
-          <h3>Listview images from Caltech 101 dataset</h3>
-          L. Fei-Fei, R. Fergus and P. Perona. Learning generative visual models from few training examples: an incremental Bayesian approach tested on 101 object categories. IEEE. CVPR 2004, Workshop on Generative-Model Based Vision. 2004
-        </div>
+          <div>
+            <h3>Listview images from Caltech 101 dataset</h3>
+            L. Fei-Fei, R. Fergus and P. Perona. Learning generative visual models from few training examples: an incremental Bayesian approach tested on 101 object categories. IEEE. CVPR 2004, Workshop on Generative-Model Based Vision. 2004
+          </div>
 
-        <div>
-          <h3>Placeholder texts generated at <a href="https://www.lipsum.com/">www.lipsum.com/</a></h3>
+          <div>
+            <h3>Placeholder texts generated at <a href="https://www.lipsum.com/">www.lipsum.com/</a></h3>
+          </div>
         </div>
       </AppBody>
     );
